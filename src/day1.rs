b28c54ef53a111e530f028a216daae1dc9ee546e
xxorza <1442523+xorza@users.noreply.github.com>
2023-12-14 13:24:42 +0200

    Initialize "advent_of_code" project with day1 solution
    The commit includes setup of the new Rust project named "advent_of_code". The project setup covers addition of the necessary configuration and metadata files including ".gitignore" and "Cargo.toml". The main logic is implemented in a new file, "day1.rs", which represents solution of the first problem/day in the challenge. The "lib.rs" also includes a reference to the "day1.rs" module.

---
+++ src/day1.rs
@@ -0,0 +1,81 @@
+fn solve(input: &str) -> u32 {
+    input
+        .lines()
+        .map(|line| {
+            let digits = find_digits(line);
+            let min_digit = digits.iter().min_by_key(|&(_, line_index)| line_index).unwrap().0;
+            let max_digit = digits.iter().max_by_key(|&(_, line_index)| line_index).unwrap().0;
+            min_digit * 10 + max_digit
+        })
+        .sum()
+}
+const DIGITS: [&str; 10] = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+
+fn find_digits(line: &str) -> Vec<(u32, usize)> {
+    DIGITS
+        .iter()
+        .enumerate()
+        .flat_map(move |(digit_index, &digit)| {
+            line
+                .match_indices(digit)
+                .map(move |(line_index, _)| (digit_index as u32, line_index))
+        })
+        .chain(
+            line
+                .chars()
+                .enumerate()
+                .filter_map(|(line_index, c)| {
+                    c
+                        .to_digit(10)
+                        .and_then(|digit| Some((digit, line_index)))
+                })
+        )
+        .collect()
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works0() {
+        const TEST_INPUT: &str = "\
+1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet";
+        let result = solve(TEST_INPUT);
+        assert_eq!(result, 142);
+    }
+
+    #[test]
+    fn it_works1() {
+        const TEST_INPUT: &str = "\
+two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen";
+        let result = solve(TEST_INPUT);
+        assert_eq!(result, 281);
+    }
+
+    #[test]
+    fn it_works1_1() {
+        const TEST_INPUT: &str = "vggvnhqkjseventwo4onetwonftrnd";
+        let result = solve(TEST_INPUT);
+        assert_eq!(result, 72);
+    }
+
+
+    #[test]
+    fn input_test() {
+        const INPUT: &str = include_str!("../inputs/day1.txt");
+        let result = solve(INPUT);
+        assert_eq!(result, 54203);
+    }
+}
+
